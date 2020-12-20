e356fab1b0bb14c8e7d0d5eaf4923e33bfed310d
Hao-Ting Li <maniac@csie.io>
2020-12-21 02:57:38 +0800

    add configuration files

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    title: 'Python 科學運算筆記',
+    description: 'Scientific Python Note',
+    base: '/scientific-python-note/',
+    repo: 'https://github.com/aquastripe/scientific-python-note/',
+    head: [
+        ['meta', { name: "viewport", content: "width=device-width,user-scaleble=0,initial-scale=1.0,maximum-scale=1.0" }],
+      ],
+      locales: {
+        '/': {
+          lang: 'zh-TW',
+        }
+      },
+    themeConfig: {
+      sidebarDepth: 0,
+      sidebar: [
+        ['/', '前言']
+      ],
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Github', link: 'https://github.com/aquastripe/scientific-python-note/' }
+      ]
+    }
+  }
